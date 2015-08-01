9047529a6aae65b9fe370fc86b2fbf08d614432c
domingosdepaola <domingosdepaola@gmail.com>
2015-08-01 04:31:44 -0300

    Primeira versao

--- OnibusWPhone/MainPage.xaml.cs
+++ OnibusWPhone/MainPage.xaml.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -27,6 +34,12 @@ namespace OnibusWPhone
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
+            myMap.Loaded += myMap_Loaded;
+        }
+
+        void myMap_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
 
         /// <summary>
@@ -43,6 +56,39 @@ namespace OnibusWPhone
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
+        }
+
+        private async void btnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+            string numeroLinha = txtNumero.Text;
+            var task = RequestAPI.GetString("http://192.168.0.12/OnibusAPI/api/onibus/getOnibusOpen?numeroLinha=" + numeroLinha);
+            var items = await task;
+            List<Onibus> lstOnibus = (List<Onibus>)items;
+            AddPins(lstOnibus);
+        }
+        
+        private void AddPins(List<Onibus> lstOnibus)
+        {
+            foreach (Onibus item in lstOnibus)
+            {
+                MapIcon MapIcon1 = new MapIcon();
+                MapIcon1.Location = new Geopoint(new BasicGeoposition()
+                {
+                    Latitude = item.Latitude,
+                    Longitude = item.Longitude
+                });
+                MapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+                MapIcon1.Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/busRed.png"));
+                MapIcon1.Title = "Onibus " + item.Linha + " Numero: " + item.Ordem;
+
+                myMap.MapElements.Add(MapIcon1);
+                myMap.Center = new Geopoint(new BasicGeoposition()
+                {
+                    Latitude = item.Latitude,
+                    Longitude = item.Longitude
+                });
+            }
+            myMap.ZoomLevel = 15;
         }
     }
 }
---
+++ OnibusWPhone/Onibus.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnibusWPhone
+{
+    public class Onibus
+    {
+        [JsonProperty("DataHora")]
+        public DateTime DataHora { get; set; }
+
+        [JsonProperty("Ordem")]
+        public string Ordem { get; set; }
+
+        [JsonProperty("Linha")]
+        public string Linha { get; set; }
+
+        [JsonProperty("Latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("Longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("Velocidade")]
+        public int Velocidade { get; set; }
+    }
+}
